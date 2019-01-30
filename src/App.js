63f42aa3134bdeba9b56012d2dc5ac1524e0265f
guillaume <guillaumeval.techer@gmail.com>
2019-01-30 14:07:15 +0100

    adds sections and subsections

--- src/App.js
+++ src/App.js
@@ -10,9 +10,16 @@ const Title = styled.h1`
 `;
 
 const Date = styled.p`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
+  font-size: 0.8rem;
+`;
+
+const Content = styled.p`
+  font-size: 1.2rem;
+`;
+
+const ExperienceTitle =styled.h1`
+  font-size: 1.5rem;
+  font-weight: 600;
 `;
 
 const Center = styled.div`
@@ -21,29 +28,46 @@ const Center = styled.div`
 `;
 
 const SubTitle = styled.div`
-  padding-left: 0.3em;
-  padding-right: 0.3em;
-  margin-left: 0.3em;
-  margin-right: 0.3em;
+  padding-left: 0.3rem;
+  padding-right: 0.3rem;
+  margin-left: 0.3rem;
+  margin-right: 0.3rem;
   border-style: solid;
-  border-width: 0.2em;
+  border-width: 0.3rem;
   font-size: 2rem;
   color: White;
 `;
 
+const SectionIntro = styled.p`
+  font-size: 1.2rem;
+`;
+
 const SectionTitle = styled(SubTitle)`
-  padding-left: 0.3em;
-  padding-right: 0.3em;
-  margin-top: 3rem ;
+  padding-left: 0.3rem;
+  padding-right: 0.3rem;
+  margin-left: 0rem;
+  margin-right: 0rem;
+  margin-top: 3rem;
   margin-bottom: 2rem;
   color: hsl(236, 51%, 36%);
 `;
 
+const SubSectionTitle = styled.div`
+  padding-left: 0.3rem;
+  padding-right: 0.3rem;
+  margin-left: 0rem;
+  margin-right: 0rem;
+  margin-top: 3rem;
+  margin-bottom: 2rem;
+  color: hsl(236, 51%, 36%);
+  border-bottom: 0.2rem solid;
+  font-size: 1.5rem;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <body>
         <section class="hero is-primary is-fullheight">
           <div class="hero-body">
             <div class="container">
@@ -66,8 +90,8 @@ class App extends Component {
           </div>
         </section>
         <Soft />
-        <Graphic/>
-      </div>
+        <Graphic />
+      </body>
     );
   }
 }
@@ -75,45 +99,86 @@ class App extends Component {
 class Soft extends Component {
   render() {
     return (
+      <section className="section">
       <div class="container">
         <SectionTitle id="engineer">Software engineer</SectionTitle>
-        <p>Magna occaecat labore incididunt ipsum elit ipsum et excepteur voluptate fugiat. Ipsum ullamco consectetur in anim cupidatat sunt culpa. Aliqua voluptate ullamco nisi commodo commodo ut aliqua consequat velit. Laboris laborum quis mollit tempor amet fugiat quis amet velit laborum eiusmod elit cupidatat. Dolore duis esse fugiat mollit pariatur amet nisi elit magna qui mollit proident eiusmod. Ad aliquip ipsum enim consequat deserunt pariatur. Incididunt est mollit reprehenderit ullamco duis.
+        <SectionIntro>
+        Cupidatat amet mollit ex elit pariatur fugiat.
+        </SectionIntro>
+        <SubSectionTitle>Technologies</SubSectionTitle>
+        <SubSectionTitle>Experiences</SubSectionTitle>
+        <Experience
+          title="Test"
+          image="https://bulma.io/images/placeholders/128x128.png"
+          text="Incididunt commodo ut ex officia fugiat elit exercitation cupidatat aliqua deserunt deserunt laboris officia."
+          date="01/02/03 - 01/02/03"
+        />
+        <Experience
+          title="Test"
+          image="https://bulma.io/images/placeholders/128x128.png"
+          text="Reprehenderit laborum nostrud non aliqua excepteur."
+          date="01/02/03 - 01/02/03"
+        />
+        <Experience
+          title="Test"
+          image="https://bulma.io/images/placeholders/128x128.png"
+          text="Eiusmod dolor tempor aute pariatur culpa elit commodo."
+          date="01/02/03 - 01/02/03"
+        />
+        <Experience
+          title="Test"
+          image="https://bulma.io/images/placeholders/128x128.png"
+          text="Tempor nostrud ea laboris veniam deserunt adipisicing magna minim sint ex occaecat."
+          date="01/02/03 - 01/02/03"
+        />
+        <SubSectionTitle>Projects</SubSectionTitle>
+        <Experience
+          title="Test"
+          image="https://bulma.io/images/placeholders/128x128.png"
+          text="Tempor nostrud ea laboris veniam deserunt adipisicing magna minim sint ex occaecat."
+          date="01/02/03 - 01/02/03"
+        />
+      </div>
+      </section>
+    );
+  }
+}
 
-Ad ea quis ad labore occaecat sit qui id nisi officia eiusmod. Nisi Lorem dolore nulla ex exercitation ad consequat quis consectetur excepteur. Reprehenderit laboris voluptate cillum aliqua quis anim id veniam ad labore fugiat id. Excepteur ad commodo deserunt eiusmod elit cupidatat. Ipsum consequat exercitation elit incididunt elit consectetur in fugiat ut anim est mollit cillum. Quis culpa exercitation labore do fugiat cillum exercitation. Lorem nisi ipsum laboris consectetur duis magna veniam sit nulla voluptate non duis.
-
-In labore ea veniam Lorem exercitation cupidatat incididunt esse commodo sit. Dolore reprehenderit qui elit esse consequat. Elit eiusmod est laboris non minim magna quis consectetur in. Magna sit pariatur velit velit Lorem ut in consequat incididunt. Id enim mollit veniam ut voluptate aute. Proident excepteur ex labore in nostrud id amet et magna cillum consequat labore incididunt sint.
-
-Anim dolore anim ex adipisicing id tempor amet veniam sint sit. Quis do quis magna do. Consequat enim eu aute culpa sunt officia. Culpa id non culpa consectetur excepteur qui. Nisi adipisicing ipsum non laboris excepteur Lorem cupidatat amet dolor irure cupidatat amet officia irure. Incididunt do excepteur cupidatat exercitation labore aliquip laborum aute.
-
-Eiusmod non duis voluptate magna. Duis in et commodo sint. Incididunt deserunt anim velit qui qui aute id cupidatat. Qui mollit cillum commodo anim pariatur ipsum. Non sunt ipsum irure ex sint in.
-
-Fugiat excepteur est cillum sint aliqua occaecat amet Lorem laboris. Aliquip duis irure nisi sunt eiusmod. Nisi sit eu deserunt quis nisi est amet aliquip. Enim consequat id duis mollit anim quis dolor officia mollit pariatur laboris et non Lorem. Fugiat anim occaecat minim nostrud elit reprehenderit incididunt ut. Laborum id aliquip aute dolore in ut in exercitation quis non labore labore magna exercitation.
-
-Est ad nulla ex veniam cupidatat ea aute culpa anim labore irure deserunt non sit. Sint occaecat ex fugiat amet veniam fugiat ad sit ex consectetur. Exercitation duis irure anim cillum aliqua do laboris. Pariatur elit qui laboris qui quis mollit cillum anim ipsum reprehenderit quis. Ut officia nulla aliqua qui voluptate ut veniam eiusmod pariatur in veniam qui qui quis.</p>
+class Experience extends Component {
+  render() {
+    return (
+      
+      <div className="columns">
+        <div className="column is-narrow ">
+          <figure class="image is-128x128">
+            <img src={this.props.image} />
+          </figure>
+        </div>
+        <div className="column">
+          <ExperienceTitle className="has-text-primary">{this.props.title}</ExperienceTitle>
+          <Date>{this.props.date}</Date>
+          <Content>{this.props.text}</Content>
+        </div>
       </div>
     );
   }
 }
+
 class Graphic extends Component {
   render() {
     return (
+      <section className="section">
+
       <div class="container">
         <SectionTitle id="graphic">Graphic design </SectionTitle>
-        <p>Magna occaecat labore incididunt ipsum elit ipsum et excepteur voluptate fugiat. Ipsum ullamco consectetur in anim cupidatat sunt culpa. Aliqua voluptate ullamco nisi commodo commodo ut aliqua consequat velit. Laboris laborum quis mollit tempor amet fugiat quis amet velit laborum eiusmod elit cupidatat. Dolore duis esse fugiat mollit pariatur amet nisi elit magna qui mollit proident eiusmod. Ad aliquip ipsum enim consequat deserunt pariatur. Incididunt est mollit reprehenderit ullamco duis.
-
-Ad ea quis ad labore occaecat sit qui id nisi officia eiusmod. Nisi Lorem dolore nulla ex exercitation ad consequat quis consectetur excepteur. Reprehenderit laboris voluptate cillum aliqua quis anim id veniam ad labore fugiat id. Excepteur ad commodo deserunt eiusmod elit cupidatat. Ipsum consequat exercitation elit incididunt elit consectetur in fugiat ut anim est mollit cillum. Quis culpa exercitation labore do fugiat cillum exercitation. Lorem nisi ipsum laboris consectetur duis magna veniam sit nulla voluptate non duis.
-
-In labore ea veniam Lorem exercitation cupidatat incididunt esse commodo sit. Dolore reprehenderit qui elit esse consequat. Elit eiusmod est laboris non minim magna quis consectetur in. Magna sit pariatur velit velit Lorem ut in consequat incididunt. Id enim mollit veniam ut voluptate aute. Proident excepteur ex labore in nostrud id amet et magna cillum consequat labore incididunt sint.
-
-Anim dolore anim ex adipisicing id tempor amet veniam sint sit. Quis do quis magna do. Consequat enim eu aute culpa sunt officia. Culpa id non culpa consectetur excepteur qui. Nisi adipisicing ipsum non laboris excepteur Lorem cupidatat amet dolor irure cupidatat amet officia irure. Incididunt do excepteur cupidatat exercitation labore aliquip laborum aute.
-
-Eiusmod non duis voluptate magna. Duis in et commodo sint. Incididunt deserunt anim velit qui qui aute id cupidatat. Qui mollit cillum commodo anim pariatur ipsum. Non sunt ipsum irure ex sint in.
-
-Fugiat excepteur est cillum sint aliqua occaecat amet Lorem laboris. Aliquip duis irure nisi sunt eiusmod. Nisi sit eu deserunt quis nisi est amet aliquip. Enim consequat id duis mollit anim quis dolor officia mollit pariatur laboris et non Lorem. Fugiat anim occaecat minim nostrud elit reprehenderit incididunt ut. Laborum id aliquip aute dolore in ut in exercitation quis non labore labore magna exercitation.
-
-Est ad nulla ex veniam cupidatat ea aute culpa anim labore irure deserunt non sit. Sint occaecat ex fugiat amet veniam fugiat ad sit ex consectetur. Exercitation duis irure anim cillum aliqua do laboris. Pariatur elit qui laboris qui quis mollit cillum anim ipsum reprehenderit quis. Ut officia nulla aliqua qui voluptate ut veniam eiusmod pariatur in veniam qui qui quis.</p>
-     
+        <SectionIntro>
+          Proident anim occaecat dolor deserunt ullamco duis dolore Lorem incididunt nostrud velit.
+        </SectionIntro>
+        <SubSectionTitle>Photos</SubSectionTitle>
+        <SubSectionTitle>Posters</SubSectionTitle>
+        <SubSectionTitle>3D</SubSectionTitle>
       </div>
+      </section>
     );
   }
 }
