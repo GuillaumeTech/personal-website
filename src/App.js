1ac8b213dd75559d0c884b8d583de72e23e97669
Guillaume TECHER <guillaume.techer@ibm.com>
2019-06-17 09:21:38 +0200

    adds animation and change layout a bit

--- src/App.js
+++ src/App.js
@@ -1,103 +1,38 @@
 import React, { Component } from "react";
-import styled, { css } from "styled-components";
+import {Title,Center,SubSectionTitle,SectionIntro,SectionTitle,SubTitle,ImagesList,ExperienceTitle,Date,Content} from "./CssComponents.js"
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
-const Title = styled.h1`
-  font-size: 5rem;
-  color: White;
-  text-align: center;
-  font-family: "Source Sans Pro", sans-serif;
-  font-weight: 900;
-`;
-
-const Date = styled.p`
-  font-size: 0.8rem;
-`;
-
-const Content = styled.p`
-  font-size: 1.2rem;
-`;
-
-const ExperienceTitle = styled.h1`
-  font-size: 1.5rem;
-  font-weight: 600;
-`;
-
-const Center = styled.div`
-  text-align: center;
-  font-size: 2rem;
-`;
-
-const SubTitle = styled.div`
-  padding-left: 0.3rem;
-  padding-right: 0.3rem;
-  margin-left: 0.3rem;
-  margin-right: 0.3rem;
-  border-style: solid;
-  border-width: 0.3rem;
-  font-size: 2rem;
-  color: White;
-`;
-
-const SectionIntro = styled.p`
-  font-size: 1.2rem;
-`;
-
-const SectionTitle = styled(SubTitle)`
-  padding-left: 0.3rem;
-  padding-right: 0.3rem;
-  margin-left: 0rem;
-  margin-right: 0rem;
-  margin-top: 3rem;
-  margin-bottom: 2rem;
-  color: hsl(236, 51%, 36%);
-`;
-
-const SubSectionTitle = styled.div`
-  padding-left: 0.3rem;
-  padding-right: 0.3rem;
-  margin-left: 0rem;
-  margin-right: 0rem;
-  margin-top: 3rem;
-  margin-bottom: 2rem;
-  color: hsl(236, 51%, 36%);
-  border-bottom: 0.2rem solid;
-  font-size: 1.5rem;
-`;
-
-const ImagesList = styled.div`
-display: flex;
-flex-wrap: wrap;
-justify-content: space-around;
-`;
+const SmoothScroll = () => (
+  <body>
+    <section class="hero is-primary is-fullheight">
+      <div class="hero-body">
+        <div class="container">
+          <Title> Hi, I'm Guillaume</Title>
+          <Center>
+            I'm thrilled by the opportunity to learn. I'm a
+            <SubTitle className="hvr-sweep-to-right">
+              {" "}
+              <AnchorLink offset='80' href="#engineer">Software engineer</AnchorLink>
+          
+            </SubTitle>
+            interested in web development, machine learning and big-data.{" "}
+            <br />
+            I'm also a
+            <SubTitle className="hvr-sweep-to-right">
+              {" "}
+              <AnchorLink offset='80' href="#graphic">Graphic design enthusiast </AnchorLink >
+            </SubTitle>
+          </Center>
+        </div>
+      </div>
+    </section>
+    <Soft />
+    <Graphic />
+  </body>
+);
 class App extends Component {
   render() {
-    return (
-      <body>
-        <section class="hero is-primary is-fullheight">
-          <div class="hero-body">
-            <div class="container">
-              <Title> Hi, I'm Guillaume</Title>
-              <Center>
-                I'm thrilled by the opportunity to learn. I'm a
-                <SubTitle className="hvr-sweep-to-right">
-                  {" "}
-                  <a href="#engineer">Software engineer</a>{" "}
-                </SubTitle>
-                interested in web development, machine learning and android.{" "}
-                <br />
-                I'm also a
-                <SubTitle className="hvr-sweep-to-right">
-                  {" "}
-                  <a href="#graphic">Graphic design enthusiast </a>
-                </SubTitle>
-              </Center>
-            </div>
-          </div>
-        </section>
-        <Soft />
-        <Graphic />
-      </body>
-    );
+  return(<SmoothScroll/>)
   }
 }
 
@@ -106,70 +41,74 @@ class Soft extends Component {
     return (
       <section className="section">
         <div class="container">
+          
           <SectionTitle id="engineer">Software engineer</SectionTitle>
           <SectionIntro>
             Cupidatat amet mollit ex elit pariatur fugiat.
           </SectionIntro>
-          <SubSectionTitle>Technologies</SubSectionTitle>
+          <div class="columns" >
+          <div class="column" >
+
+          <SubSectionTitle>About Me</SubSectionTitle>
           <ImagesList>              <img
                 src={process.env.PUBLIC_URL + "/py_logo.svg"}
                 alt="Monkey face"
-                style={{ padding: "0.5rem", width: "auto", height: "150px" }}
+                style={{ padding: "0.5rem", width: "auto", height: "70px" }}
               />
               <img
                 src={process.env.PUBLIC_URL + "/C_logo.svg"}
                 alt="Monkey face"
-                style={{  padding: "0.5rem", width: "auto", height: "150px" }}
+                style={{  padding: "0.5rem", width: "auto", height: "70px" }}
               />
               <img
                 src={process.env.PUBLIC_URL + "/js_logo.svg"}
                 alt="Monkey face"
-                style={{  padding: "0.5rem", width: "auto", height: "150px" }}
+                style={{  padding: "0.5rem", width: "auto", height: "70px" }}
               />
               <img
                 src={process.env.PUBLIC_URL + "/linux_logo.svg"}
                 alt="Monkey face"
-                style={{  padding: "0.5rem", width: "auto", height: "150px" }}
+                style={{  padding: "0.5rem", width: "auto", height: "70px" }}
               />
               <img
                 src={process.env.PUBLIC_URL + "/Matlab_Logo.png"}
                 alt="Monkey face"
-                style={{  padding: "0.5rem", width: "auto", height: "150px" }}
+                style={{  padding: "0.5rem", width: "auto", height: "70px" }}
               />
               </ImagesList>
-
+          </div>
+          <div class="column" >
           <SubSectionTitle>Experiences</SubSectionTitle>
+          <div class="timeline">
           <Experience
             title="Test"
-            image="https://bulma.io/images/placeholders/128x128.png"
             text="Incididunt commodo ut ex officia fugiat elit exercitation cupidatat aliqua deserunt deserunt laboris officia."
             date="01/02/03 - 01/02/03"
           />
           <Experience
             title="Test"
-            image="https://bulma.io/images/placeholders/128x128.png"
             text="Reprehenderit laborum nostrud non aliqua excepteur."
             date="01/02/03 - 01/02/03"
           />
           <Experience
             title="Test"
-            image="https://bulma.io/images/placeholders/128x128.png"
             text="Eiusmod dolor tempor aute pariatur culpa elit commodo."
             date="01/02/03 - 01/02/03"
           />
           <Experience
             title="Test"
-            image="https://bulma.io/images/placeholders/128x128.png"
             text="Tempor nostrud ea laboris veniam deserunt adipisicing magna minim sint ex occaecat."
             date="01/02/03 - 01/02/03"
           />
+          </div>
           <SubSectionTitle>Projects</SubSectionTitle>
           <Experience
             title="Test"
-            image="https://bulma.io/images/placeholders/128x128.png"
             text="Tempor nostrud ea laboris veniam deserunt adipisicing magna minim sint ex occaecat."
             date="01/02/03 - 01/02/03"
           />
+          </div>
+          </div>
         </div>
       </section>
     );
@@ -179,21 +118,20 @@ class Soft extends Component {
 class Experience extends Component {
   render() {
     return (
-      <div className="columns">
-        <div className="column is-narrow ">
-          <figure class="image is-128x128">
-            <img src={this.props.image} />
-          </figure>
-        </div>
-        <div className="column">
-          <ExperienceTitle className="has-text-primary">
-            {this.props.title}
-          </ExperienceTitle>
-          <Date>{this.props.date}</Date>
-          <Content>{this.props.text}</Content>
-        </div>
-      </div>
-    );
+      <div>
+        <div class="timeline-block timeline-block-right">
+          <div class="marker"></div>
+            <div class="timeline-content">
+              <ExperienceTitle className="has-text-primary">
+                {this.props.title}
+              </ExperienceTitle>
+              <Date>{this.props.date}</Date>
+              <Content>{this.props.text}</Content>
+            </div>
+            </div>
+
+          </div>);
+     
   }
 }
 
@@ -216,11 +154,5 @@ class Graphic extends Component {
   }
 }
 
-
-class Galery extends Component{
-  render() {
-    return (<p>test</p>{this.state.contents.map((element, index) => ())};
-  }
-}
 
 export default App;
---
+++ src/CssComponents.js
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  font-size: 5rem;
+  color: White;
+  text-align: center;
+  font-family: "Source Sans Pro", sans-serif;
+  font-weight: 900;
+  animation: tracking-in-contract 0.5s cubic-bezier(0.215, 0.610, 0.355, 1.000) both;
+  animation-delay: 0.1s;
+  white-space: nowrap;
+`;
+
+export const Date = styled.p`
+  font-size: 0.8rem;
+`;
+
+export const Content = styled.p`
+  font-size: 1.2rem;
+`;
+
+export const ExperienceTitle = styled.h1`
+  font-size: 1.5rem;
+  font-weight: 600;
+`;
+
+export const Center = styled.div`
+  text-align: center;
+  font-size: 2rem;
+  animation: reveal 0.7s cubic-bezier(0.215, 0.610, 0.355, 1.000) both;
+  animation-delay: 0.5s;
+`;
+
+export const SubTitle = styled.div`
+  padding-left: 0.3rem;
+  padding-right: 0.3rem;
+  margin-left: 0.3rem;
+  margin-right: 0.3rem;
+  border-style: solid;
+  border-width: 0.3rem;
+  font-size: 2rem;
+  color: White;
+`;
+
+export const SectionIntro = styled.p`
+  font-size: 1.2rem;
+`;
+
+export const SectionTitle = styled(SubTitle)`
+  padding-left: 0.3rem;
+  padding-right: 0.3rem;
+  margin-left: 0rem;
+  margin-right: 0rem;
+  margin-top: 3rem;
+  margin-bottom: 2rem;
+  color: hsl(236, 51%, 36%);
+`;
+
+export const SubSectionTitle = styled.div`
+  padding-left: 0.3rem;
+  padding-right: 0.3rem;
+  margin-left: 0rem;
+  margin-right: 0rem;
+  margin-top: 3rem;
+  margin-bottom: 2rem;
+  color: hsl(236, 51%, 36%);
+  border-bottom: 0.2rem solid;
+  font-size: 1.5rem;
+`;
+
+export const ImagesList = styled.div`
+display: flex;
+flex-wrap: wrap;
+justify-content: space-around;
+`;
